a78ce9f5a4b648de7ab96fc78fda703e72633f96
George Hammond <george.hammond@amalitech.org>
2022-06-04 05:06:04 +0000

    Header- shortly

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Nav from '../Nav/nav'
+import Nav from '../Nav/nav';
+import Header from '../Header/header';
 
 const App = () => {
   return (
     <div>
       <Nav />
+      <Header />
     </div>
   )
 }
