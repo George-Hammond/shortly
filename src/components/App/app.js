9f4277f96ea81e2627ef506ff3423779c1c0bac0
George Hammond <george.hammond@amalitech.org>
2022-06-06 20:04:56 +0000

    Footer- complete

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Shortner from '../Forms/urlShortner/shortner';
 import Header from '../Header/header';
+import Footer from '../Footer/footer';
 
 const App = () => {
   return (
     <div>      
       <Header />
       <Shortner />
+      <Footer />
     </div>
   )
 }
