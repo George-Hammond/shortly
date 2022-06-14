a9fbc3857c7a049188102f3330c4a207d30743d7
George Hammond <george.hammond@amalitech.org>
2022-06-14 17:18:51 +0000

    link shortening functionality

--- src/components/Forms/shortened link lists/listedLinks.js
+++ src/components/Forms/shortened link lists/listedLinks.js
@@ -1,24 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './listedLinks.css';
+import axios from 'axios';
 
-const ListedLinks = () => {
-const [shortenLink, setShorenLink]= useState("https://rel.ink/k4lKyk");
-const [copied, setCopied] = useState(false)
+const ListedLinks = ({valueInput, toggle_linkBar}) => {
+    console.log(valueInput)
+    const [shortenLink, setShortenLink]= useState([]);
+    console.log(shortenLink)
+    const [copied, setCopied] = useState(false);
+    console.log(toggle_linkBar);
+    
+
+    useEffect(()=>{
+        const timer =setTimeout(()=>{
+            setCopied(false);
+        }, 2000);
+
+        return ()=> clearTimeout(timer)
+    }, [copied])
+    const getShortenLinkData = async () =>{
+        try{
+            const response = await axios(`https://api.shrtco.de/v2/shorten?url=${valueInput}`);
+
+            setShortenLink([response.data.result.full_short_link]);
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    useEffect(()=>{
+        if(valueInput.length){
+            getShortenLinkData();
+        }
+    }, [])
 
   return (
     <div>
-      <div className='listedLinks-container'>
-        <div className='listedLink'>
-            <p id='url-link'>https://www.frontendmentor.io</p>
-            <div className='right'>
-                <p id='link'>{shortenLink}</p>
-                <CopyToClipboard text={shortenLink} onCopy={()=> setCopied(true)}>
-                    <button id={copied ? 'copied' : 'copy'} >{copied ? 'Copied!' : 'Copy'}</button>
-                </CopyToClipboard>                    
-            </div>
-        </div>               
-      </div>
+    <div className='listedLinks-container'>
+        <div className={toggle_linkBar ? 'listedLink' : 'listedLinkDisappear'}>
+            <p id='url-link'>{valueInput}</p>
+                 <div className='right'>     
+                    <p id='link'>{shortenLink}</p>
+                     <CopyToClipboard text={shortenLink} onCopy={()=> setCopied(true)}>
+                         <button id={copied ? 'copied' : 'copy'} >{copied ? 'Copied!' : 'Copy'}</button>     
+                     </CopyToClipboard>                                    
+                 </div>     
+        </div>                              
+    </div>           
     </div>
   )
 }
